8f63c69851d00c47840fb4faa837445b626ebf67
Jiaxi Wen <taurreswen@gmail.com>
2022-04-15 16:29:21 -0400

    Add middleware for error handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// handle json body
+app.use(express.json())
+
+// handle 404
+app.use(notFound)
+
+// handle 500
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 4000
 
