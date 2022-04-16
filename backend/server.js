2ed83f618a0ae8f2ea0b893a96d8bcdc2c32d3f4
Jiaxi Wen <taurreswen@gmail.com>
2022-04-16 00:33:32 -0400

    Add connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './util/connectDB.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
-import userRoute from './route/userRoute.js'
-
 
 dotenv.config()
 
@@ -21,9 +19,6 @@ app.use(notFound)
 
 // handle 500
 app.use(errorHandler)
-
-app.use('/api/users', userRoute)
-
 
 const PORT = process.env.PORT || 4000
 
