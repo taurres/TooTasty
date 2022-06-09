cf54f9471d4ca4d8f7732a144f51ef8d400beaae
Kexin Qiu <kristenqiu19@gmail.con>
2022-06-08 20:16:42 -0400

    test

--- frontend/src/services/reviewService.js
+++ frontend/src/services/reviewService.js
@@ -1,4 +1,4 @@
-// import axios from 'axios';
+import axios from 'axios';
 //
 // const API_BASE = 'https://cs5610server.herokuapp.com/api' || 'http://localhost:4000/api';
 // const TUITS_API = `${API_BASE}/tuits`;
